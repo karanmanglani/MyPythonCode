9dbdfec1aaa8afe8d98406d2af5a066bf3632882
Karan Manglani <bkn45for@gmail.com>
2020-11-26 10:00:19 +0530

    Added Setup.exe and changes to kzip and Tic tac toe

--- Project Applications/KZip/main.py
+++ Project Applications/KZip/main.py
@@ -1,2 +1,22 @@
 import tkinter as tk
 from tkinter import ttk , filedialog , messagebox
+import os
+
+mainApplication = tk.Tk()
+mainApplication.geometry('1200x800')
+mainApplication.title('Kzip file Archiver')
+mainApplication.wm_iconbitmap('icon.ico')
+
+####################### Main Menu ##################################
+
+#-------------&& End of main menu && -----------------------------#
+
+##################### Body ########################################
+
+#------------------&& End of body &&------------------------------#
+
+################## Working of main menu ###########################
+
+#---------------- && End &&---------------------------------------#
+
+mainApplication.mainloop()
