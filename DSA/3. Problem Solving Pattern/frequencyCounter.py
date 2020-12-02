29db31f9c8fd61f3e2b42d34b010a3e7abac843e
Karan Manglani <bkn45for@gmail.com>
2020-12-01 21:40:54 -0800

    Understood Frequency Counter Pattern

--- DSA/3. Problem Solving Pattern/frequencyCounter.py
+++ DSA/3. Problem Solving Pattern/frequencyCounter.py
@@ -10,10 +10,58 @@
 # same([1,2,3] , [1,9]) ==> false
 # same([1,2,1] , [4,1,4]) ==> false
 
+######## Naive approach without frequency counter design pattern with O(n^2) complexity because of nested Loops
 arr1 = [1,2,3]
 arr2 = [4,1,9]
 
 def same(arr1, arr2):
+    if not(len(arr1) == len(arr2)):
+        return False
     
+    for i in arr1:
+        squareFound = False
+        for j in arr2:
+            if (j == i*i):
+                squareFound = True
+                arr2.remove(j)
+        if squareFound:
+            squareFound = False
+            break
+        else:
+            return False
+        
+    return True
 
 print(same(arr1,arr2))
+
+############# Using Frequency counter pattern with time complexity O(n)
+arr1 = [1,2,3]
+arr2 = [4,1,9]
+def sameRefactored(arr1,arr2):
+    if not(len(arr2) == len(arr1)):
+        return False
+    
+    frequencyCounter1 = {}
+    frequencyCounter2 = {}
+
+    for i in arr1:
+        if (i in frequencyCounter1.keys()):
+            frequencyCounter1[i] += 1
+        else:
+            frequencyCounter1[i] = 1
+
+    for j in arr2:
+        if (j in frequencyCounter2.keys()):
+            frequencyCounter2[j] += 1
+        else:
+            frequencyCounter2[j] = 1
+    
+    for i in frequencyCounter1.keys():
+        if not(i*i in frequencyCounter2.keys()):
+            return False
+        if not(frequencyCounter1[i] == frequencyCounter2[i*i]):
+            return False
+    
+    return True
+
+print(sameRefactored(arr1,arr2))
