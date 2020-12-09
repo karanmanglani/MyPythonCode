5dd360d5c57b0d7ffce0d079159d18831b305c4e
Karan Manglani <bkn45for@gmail.com>
2020-12-09 20:08:50 +0530

    Understood transposition cipher

--- Cryptography/3. transpositionCipher.py
+++ Cryptography/3. transpositionCipher.py
@@ -1,4 +1,4 @@
-import pyperclip
+import pyperclip,math
 
 def main():
     myMessage = 'Common sense is not so common'
@@ -6,6 +6,9 @@ def main():
     cipherText = encryptMessage(myKey,myMessage)
     print(cipherText + '|')
     pyperclip.copy(cipherText)
+    plainText = decryptMessage(myKey, cipherText)
+    print(plainText )
+    pyperclip.copy(plainText)
 
 def encryptMessage(key,myMessage):
     cipherText = ['']*key
@@ -15,6 +18,23 @@ def encryptMessage(key,myMessage):
             cipherText[col] += myMessage[pointer]
             pointer += key
     return ''.join(cipherText)
+
+def decryptMessage(key,encryptedMessage):
+    numberOfColumns = math.ceil(len(encryptedMessage) / key)
+    numberOfRows = key
+    shadedBoxes = (numberOfColumns * numberOfRows) - len(encryptedMessage)
+    plainText = [''] * numberOfColumns
+    col = 0
+    row = 0
+
+    for character in encryptedMessage:
+        plainText[col] += character
+        col += 1
+        if(col == numberOfColumns or (col == numberOfColumns - 1 and row >= numberOfRows- shadedBoxes)):
+            col = 0
+            row += 1
+        
+    return ''.join(plainText)
 
 if __name__ == '__main__':
     main()
