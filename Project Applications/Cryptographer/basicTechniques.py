5909ac2dacdb10fce3b893cc4e3a1da3ac2c2905
Karan Manglani <bkn45for@gmail.com>
2020-12-28 21:56:45 +0530

    Added funtionality for Affine and Multiplicative cipher to be used in Application

--- Project Applications/Cryptographer/basicTechniques.py
+++ Project Applications/Cryptographer/basicTechniques.py
@@ -1,5 +1,18 @@
 import math
 
+### Maths for Cryptography
+def gcd(a,b):
+    while a!= 0:
+        a,b = b%a,a
+    return b
+
+def modInverse(a,m):
+    a = a % m
+    for i in range(1,m):
+        if((a*i) % m == 1):
+            return i
+    return 1
+
 #### Reverse Cipher
 def reverseCipher(message):
     return message[-1:-len(message) - 1:-1]
@@ -62,13 +75,59 @@ def transpositionCipherDecrypt(message,key):
 
 ### Multipicative Cipher
 ## Multiplicative cipher Encrypt
-
+def multiplicativeCipherEncrypt(message,key):
+    encryptedMessage = ''
+    length = 128
+    if(gcd(key,length) == 1):
+        for i in message:
+            index = ord(i)
+            encryptedIndex = (index * key) % length
+            encryptedMessage += chr(encryptedIndex)
+        return encryptedMessage
+    else:
+        return 'Please Chose a key such that gcd of key and 128 is 1'
 ## Multiplicative Cipher Decrypt
+def multiplicativeCipherDecrypt(message,key):
+    decryptedMessage = ''
+    length = 128
+    if(gcd(key,length) == 1):
+        for i in message:
+            index = ord(i)
+            decryptedIndex = (modInverse(key,length) * index ) % length
+            decryptedMessage += chr(decryptedIndex)
+        return decryptedMessage
+    else:
+        return 'Please Chose a key such that gcd of key and 128 is 1'
+
+
 
 ### Affine Cipher
 ## Affine Cipher Encryption
-
+def affineCipherEncryption(message,keyList):
+    length = 128
+    encryptedMessage = ''
+    if(gcd(keyList[0] , length) == 1):
+        for i in message:
+            index = ord(i)
+            encryptedIndex = (index * keyList[0] + keyList[1]) % length
+            encryptedMessage += chr(encryptedIndex)
+        return encryptedMessage
+    else:
+        return 'Please Chose multiply key such that gcd of key and 128 is 1'
 ## Affine Cipher Decryption
+def affineCipherDecryption(message,keyList):
+    length = 128
+    decryptedMessage = ''
+    if(gcd(keyList[0] , length) == 1):
+        for i in message:
+            index = ord(i)
+            decryptedIndex = modInverse(keyList[0],length) * (index - keyList[1]) % length
+            decryptedMessage += chr(decryptedIndex)
+        return decryptedMessage
+    else:
+        return 'Please Chose multiply key such that gcd of key and 128 is 1'
+
+
 
 ### Substitution Cipher
 ## Substitution Cipher Encryption
