2489df673bd9c58a6f91557c86ef1eb4f8fc70a5
Karan Manglani <bkn45for@gmail.com>
2021-01-06 13:07:08 +0530

    Added functioning of vignere cipher and vernam cipher in application

--- Project Applications/Cryptographer/intermediateTechniques.py
+++ Project Applications/Cryptographer/intermediateTechniques.py
@@ -1,18 +1,63 @@
+### Required helper functions
+def generateKey(string,key):
+    if len(string) ==  len(key):
+        return key
+    else:
+        for i in range(len(string) - len(key)):
+            key += key[i % len(key)]
+        return key
+
+### Vernam Cipher
+## Vernam Cipher Encryption
+def VernamCipherEncrypt(text,key):
+    cipherText = ''
+    ptr = 0
+    for char in text:
+        cipherText += chr(ord(char) + ord(key[ptr]) % 128)
+        ptr += 1
+        if ptr == len(key):
+            ptr = 0
+    return cipherText
+## Vernam Cipher Decryption
+def VernamCipherDecrypt(text,key):
+    plainText = ''
+    ptr = 0
+    for char in text:
+        plainText += chr(ord(char) - ord(key[ptr]) + 128 % 128)
+        ptr += 1
+        if ptr == len(key):
+            ptr = 0
+    return plainText
+
+### Vignere Cipher 
+## Vignere Cipher Encrytion
+def VignereCipherEncrypt(text,key):
+    key = generateKey(text,key)
+    cipherText = ''
+    ptr = 0
+    for char in text:
+        cipherText += chr(ord(char) + ord(key[ptr]) % 128)
+        ptr += 1
+        if ptr == len(key):
+            ptr = 0
+    return cipherText
+
+## Vignere Cipher Decryption
+def VignereCipherDecrypt(text,key):
+    key = generateKey(text,key)
+    plainText = ''
+    ptr = 0
+    for char in text:
+        plainText += chr(ord(char) - ord(key[ptr]) + 128 % 128)
+        ptr += 1
+        if ptr == len(key):
+            ptr = 0
+    return plainText
 
 ### Playfair Cipher
 ## Playfair Cipher Encryption
 
 ## Playfair Cipher Encryption
-
-### Vernam Cipher
-## Vernam Cipher Encryption
-
-## Vernam Cipher Decryption
-
-### Vignere Cipher 
-## Vignere Cipher Encrytion
-
-## Vignere Cipher Decryption
 
 ### Base64 Encoding and Decoding
 ## Base64 Encoding
