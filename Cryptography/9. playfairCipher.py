78e9fa3141740a5e4bc24bff379e9cd569653a86
Karan Manglani <bkn45for@gmail.com>
2021-01-07 11:12:53 +0530

    Understood and programmed playfaircipher decrytion

--- Cryptography/9. playfairCipher.py
+++ Cryptography/9. playfairCipher.py
@@ -20,8 +20,7 @@ def generateKey(key):
         keyList.append(keyString[i * 5: i*5 + 5])
     return keyList
 
-def pairCreater(message):
-    
+def plainTextPairCreater(message):
     textList = []
     for i in range(0,len(message),2):
         a = message[i]
@@ -35,7 +34,7 @@ def pairCreater(message):
 
 def playfairCipherEncrypt(key,message):
     keyMatrix = generateKey(key)
-    plainTextPairs = pairCreater(message)
+    plainTextPairs = plainTextPairCreater(message)
     cipherText = ''
     for pair in plainTextPairs:
         appliedRule = False
@@ -72,4 +71,57 @@ def playfairCipherEncrypt(key,message):
             cipherText += keyMatrix[i0][j1] + keyMatrix[i1][j0]
     return cipherText
 
+def cipherTextPairCreater(cipherText):
+    textList = []
+    for i in range(0,len(cipherText),2):
+        b= cipherText[i+1]
+        a = cipherText[i]
+        if b == 'x':
+            if i+1 == len(cipherText):
+                b = ''
+            else:
+                b = a
+        textList.append(a+b)
+    return textList
+
+def playfairCipherDecryption(key,message):
+    keyMatrix = generateKey(key)
+    cipherTextPairs = cipherTextPairCreater(message)
+    plainText = ''
+    for pair in cipherTextPairs:
+        appliedRule = False
+        for row in keyMatrix:
+            if pair[0] in row and pair[1] in row:
+                j0 = row.find(pair[0])
+                j1 = row.find(pair[1])
+                plainText += row[(j0 + 4) % 5] + row[(j1 + 4) % 5]
+                appliedRule = True
+            
+        if not appliedRule:
+            for j in range(5):
+                col = ''.join(keyMatrix[i][j] for i in range(5))
+                if pair[0] in col and pair[1] in col:
+                    j0 = col.find(pair[0])
+                    j1 = col.find(pair[1])
+                    plainText += col[(j0 + 4) % 5] + col[(j1 + 4) % 5]
+                    appliedRule = True
+                    
+        if not appliedRule:
+            i0 = 0
+            j0 = 0
+            i1 = 0
+            j1 = 0
+            for i in range(5):
+                row = keyMatrix[i]
+                if pair[0] in row:
+                    i0 = i
+                    j0 = row.find(pair[0])
+                
+                if pair[1] in row:
+                    i1 = i
+                    j1 = row.find(pair[1])
+            plainText += keyMatrix[i0][j1] + keyMatrix[i1][j0]
+    return plainText
+
 print(playfairCipherEncrypt('karan','karanisgreat'))
+print(playfairCipherDecryption('karan',playfairCipherEncrypt('karan','karanisgreat')))
